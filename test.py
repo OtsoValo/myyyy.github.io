c554f4a7c149403e975cce5963eb1cc7c21b1479
Syfy <100714131@qq.com>
2017-09-13 22:41:19 +0800

    Signed-off-by: Syfy <100714131@qq.com>

---
+++ test.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    # array 二维列表
+    def Find(self, target, array):
+        # write code here
+        _index = None 
+        status = False
+        for ar in array:
+            for no ,i in enumerate(ar):
+                if target < i:
+                    if target==i:
+                        status=True
+                    else:
+                        _index=no
+        a_len = len(array)
+        print _index,a_len,range(1,a_len)
+        _list = range(_index)
+        if _index:
+            for i in range(3):
+                print i
+                for j in _list:
+                    print array[i][j]
+                    if target==array[i][j]:
+                        status=True
+		return status
+
+if  __name__ == '__main__':
+    print Solution().Find(7,[[1,2,8,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]])
