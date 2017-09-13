7cf7f70955bed2d366628c23352bb9de99472824
root <root@localhost.localdomain>
2017-09-13 23:04:58 +0800

    fix

--- test.py
+++ test.py
@@ -16,13 +16,11 @@ class Solution:
         print _index,a_len,range(1,a_len)
         _list = range(_index)
         if _index:
-            import pdb;pdb.set_trace()
             for i in range(1,a_len):
                 for j in _list:
                     print array[i][j],i,j
                     if target==array[i][j]:
                         status=True
-                        return status
-
+        return status
 if  __name__ == '__main__':
     print Solution().Find(7,[[1,2,8,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]])
